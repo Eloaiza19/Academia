f8b545db0081c71ec968de37eb31a42e1773b2de
Eloaiza19 <estebanlooss@gmail.com>
2022-08-04 09:03:08 -0500

    Primera versión del proyecto Academia. Vamos en el método store del controlador de recursos

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*Traemos toda la informacion de la tabla Cursos a través del modelo y el metodo all() */
+        $cursito = Curso::all();
+        /*Se adjunta cursito a la vista para poderlo usar */
+        return view('cursos.index', compact('cursito'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cursos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Se devuelve la peticion hecha al servidor
+        // return $request->all();
+        $cursito = new Curso(); //Crear una instancia de la clase Curso
+        $cursito->nombre = $request->input('nombre');
+        $cursito->description = $request->input('description');
+        if($request->hasFile('imagen')){
+            $cursito->imagen = $request->file('imagen')->store('public/cursos');
+        }
+        $cursito->duracion = $request->input('duracion');
+        $cursito->save();/*Con el comando save se registra la info en la bd*/
+
+        return 'Guardado exitosamente';
+        //return $request->input('nombre');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('cursos.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('cursos.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
